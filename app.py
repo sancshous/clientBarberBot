7283c273d7bf21182b7104e53954a6faf35cfa18
sancshous <shura.starkov.2014@mail.ru>
2024-07-14 16:15:40 +0400

    v1.0.2

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def book_appointment():
     cart = request.get_json()
     services = ''
     for service in cart["service"]:
-        services += str(service.id) + ','
+        services += str(service["id"]) + ','
     conn = sqlite3.connect('barbershop.db')
     cur = conn.cursor()
     cur.execute('''INSERT INTO appointments (master_id, services, date, time, user_name, user_phone, user_comment)
